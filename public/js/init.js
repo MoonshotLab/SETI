807a3c95cb19d925f0d6f97bca61f5673b6e8cf2
Tim Gillissen <info@timgillissen.com>
2014-10-16 16:50:50 -0500

    I committed first!

--- public/js/init.js
+++ public/js/init.js
@@ -665,7 +665,17 @@ var activeContent = 0;
 		}
 	});
 
-
+	app.directive('errSrc', function() {
+	  return {
+	    link: function(scope, element, attrs) {
+	      element.bind('error', function() {
+	        if (attrs.src != attrs.errSrc) {
+	          attrs.$set('src', attrs.errSrc);
+	        }
+	      });
+	    }
+	  }
+	});
 
 
 	// Menu
